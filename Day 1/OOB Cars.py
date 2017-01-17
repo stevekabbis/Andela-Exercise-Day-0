b26dd215424e30cce04b08ced643c8049fbb4499
stevekabbis <stevekabbis@yahoo.com>
2017-01-17 17:42:29 +0300

    Update OOB Cars.py

--- Day 1/OOB Cars.py
+++ Day 1/OOB Cars.py
@@ -1,13 +1,13 @@
 #A Real life Application of OOP
 class car:
-  def __init__(self, name):
+  def __init__(self, name):    # This will construct the class
     self.name = name
 
-class type_1(car):
+class type_1(car):     #create another class that inherits from car
   def type(self):
     return "convertible"
     
-class type_2(car):
+class type_2(car):    #Another class that also inherits from car
   def type(self):
     return "4 wheel drive"
     
