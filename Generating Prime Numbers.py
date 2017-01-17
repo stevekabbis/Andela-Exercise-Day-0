f235bf274ddf09ddd3bea4cfd2d086f74e011aa3
YOUR NAMEstevekabbisgit config --global user.email stevekabbis@yahoo.comgit initgit config --global user.name YOUR <stevekabbis@yahoo.com>
2017-01-17 12:35:48 +0300

    Comments on error

--- Generating Prime Numbers.py
+++ Generating Prime Numbers.py
@@ -1,4 +1,5 @@
 #"A simple way of getting prime numbers in a given range"
+#This is a trial code and seems to give me repeating numbers
 limit=int(input("Enter the limit:"))
 
 print ("Prime numbers within the limits are:")
