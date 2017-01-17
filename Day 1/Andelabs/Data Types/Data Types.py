860f70358f4c62a157c5c0f7f6509da6b79df0df
YOUR NAMEstevekabbisgit config --global user.email stevekabbis@yahoo.comgit initgit config --global user.name YOUR <stevekabbis@yahoo.com>
2017-01-17 19:36:05 +0300

    Updated the code with comments

--- Day 1/Andelabs/Data Types/Data Types.py
+++ Day 1/Andelabs/Data Types/Data Types.py
@@ -1,19 +1,17 @@
-def data_type(value):
-	if type(value) == None:
-		return 'no value'
-	if type(value) == str:
-		return len(value)
-	elif type(value) == bool:
-		return value
-	elif type(value) == int:
-		if value < 100:
-			return "less than 100"
-		elif value == 100:
-			return 'equal to 100'
-		else:
-			return "more than 100"
-	elif type(value) == list:
-		if len(value) < 3:
-			return None
-		else:
-			return value[2]
+
+def fizz_buzz(n):
+    for num in range(1,n): #identify a number between 1 and n
+
+        if num%3==0 and num%5==0: # Checking for dividiblity by 3 and 5
+	        return ("FizzBuzz")
+
+        elif num%3==0:
+	        return("Fizz")
+
+        elif num%5==0:
+	        return("Buzz")
+
+        else:
+	        return(n)
+
+if __name__=="__fizz_buzz__":fizz_buzz()
