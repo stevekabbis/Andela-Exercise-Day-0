cb406e75263360cddd4665ffd0bf873711d4118b
YOUR NAMEstevekabbisgit config --global user.email stevekabbis@yahoo.comgit initgit config --global user.name YOUR <stevekabbis@yahoo.com>
2017-01-17 12:54:16 +0300

    Modified the code

--- Generating Prime Numbers.py
+++ Generating Prime Numbers.py
@@ -1,10 +1,10 @@
 #"A simple way of getting prime numbers in a given range"
 #This is a trial code and seems to give me repeating numbers
-limit=900
 
-print ("Prime numbers within the limits are:")
+n=int(input("Enter a number:"))
+print ("Prime numbers within the limit are:")
 
-for num in range(0,limit):
+for num in range(0,n):
     if num > 1:
         for i in range(2,num):
             if (num % i) ==0:
