c4ffb2bc91a13fbbdc88445867ac81db3a862d70
YOUR NAMEstevekabbisgit config --global user.email stevekabbis@yahoo.comgit initgit config --global user.name YOUR <stevekabbis@yahoo.com>
2017-01-17 12:22:15 +0300

    Added my file

---
+++ Generating Prime Numbers.py
@@ -0,0 +1,13 @@
+
+limit=int(input("Enter the limit:"))
+
+print ("Prime numbers within the limits are:")
+
+for num in range(0,limit):
+    if num > 1:
+        for i in range(2,num):
+            if (num % i) ==0:
+                break
+            else:
+                print(num)
+    
