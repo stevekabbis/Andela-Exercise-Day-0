778d13b4b6a0a5bc3ae66c1eacd1f627b92b3d1f
stevekabbis <stevekabbis@yahoo.com>
2017-01-18 14:04:28 +0300

    Update Max and Min Number.py

--- Day 2/Max and Min Number.py
+++ Day 2/Max and Min Number.py
@@ -1,9 +1,9 @@
-def find_max_min(numbers):
-    min_max = []
+def find_max_min(numbers): #define the function that takes an argument
+    min_max = []           #assign variables to the max and min
     max_num = max(numbers)
     min_num = min(numbers)
 
-    if isinstance(numbers, list):
+    if isinstance(numbers, list):    #Identify the max and min numbers of the list
         if max_num==min_num:
             min_max.append(max_num)
             return min_max
