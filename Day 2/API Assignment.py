436aa950fbacbff2f41c8292c190634807506387
YOUR NAMEstevekabbisgit config --global user.email stevekabbis@yahoo.comgit initgit config --global user.name YOUR <stevekabbis@yahoo.com>
2017-01-18 18:59:52 +0300

    Changed the API to seek info from reddit's subreddit of history

--- Day 2/API Assignment.py
+++ Day 2/API Assignment.py
@@ -1,5 +1,5 @@
 
 import requests
 
-r = requests.get('http://www.reddit.com/r/history')
-print (r.json())
+r = requests.get('http://www.reddit.com/r/history') # will request for data from the subreddit called history
+print (r.json())    # Will give the final output
