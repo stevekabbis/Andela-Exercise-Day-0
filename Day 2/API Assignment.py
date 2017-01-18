b56b0f749e7a584e7c307ba421a2d417b1c80599
YOUR NAMEstevekabbisgit config --global user.email stevekabbis@yahoo.comgit initgit config --global user.name YOUR <stevekabbis@yahoo.com>
2017-01-18 19:01:56 +0300

    Changed website location to reddit and find the subreddit History

--- Day 2/API Assignment.py
+++ Day 2/API Assignment.py
@@ -1,5 +1,5 @@
 
-import requests
+import requests     #will import the module called requests
 
 r = requests.get('http://www.reddit.com/r/history') # will request for data from the subreddit called history
 print (r.json())    # Will give the final output
