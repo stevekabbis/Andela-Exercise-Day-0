3b950835207a2a374ee3da8d12ccc1f753ddae66
YOUR NAMEstevekabbisgit config --global user.email stevekabbis@yahoo.comgit initgit config --global user.name YOUR <stevekabbis@yahoo.com>
2017-01-18 21:43:08 +0300

    Modified the Car OOP code so that it runs okay

--- Day 1/OOB Cars.py
+++ Day 1/OOB Cars.py
@@ -1,18 +1,25 @@
-#A Real life Application of OOP
-class car:
-  def __init__(self, name):    # This will construct the class
-    self.name = name
+class Car(object):
 
-class type_1(car):     #create another class that inherits from car
-  def type(self):
-    return "convertible"
-    
-class type_2(car):    #Another class that also inherits from car
-  def type(self):
-    return "4 wheel drive"
-    
-    
-cars= [type_1("Ferrari Enzo"), type_2("Porsche Arcea")]
+    speed = 0
 
-for car in cars:
-  print(car.name +" " +car.type())
+    def __init__(self, name='General', model="GM", car_type="saloon"):
+        self.name = name
+        self.model = model
+        self.type = car_type
+        if self.name in ('Toyota', 'Nissan'):
+          self.num_of_doors = 2
+        else: self.num_of_doors = 4
+        if self.type == 'saloon':
+          self.num_of_wheels = 4
+        else: self.num_of_wheels = 8
+
+    def is_saloon(self):
+        if self.type == 'saloon':
+            return True
+
+    def drive(self, n):
+        if self.name == 'DRIVER':
+            self.speed = 77
+        else:
+            self.speed = 10 ** n
+        return self
