f88ae71093f0f64078c8418aa6ce815bc045b6da
YOUR NAMEstevekabbisgit config --global user.email stevekabbis@yahoo.comgit initgit config --global user.name YOUR <stevekabbis@yahoo.com>
2017-01-19 16:22:01 +0300

    Added extar code to make the program

--- Day 2/API Assignment.py
+++ Day 2/API Assignment.py
@@ -1,5 +1,31 @@
 
 import requests     #will import the module called requests
 
-r = requests.get('http://www.reddit.com/api/info') # will request for data from the reddit 
-print (r.json())    # Will give the final output
+response = requests.get("http://api.open-notify.org/astros.json")
+info = response.json() # will request for data from the api 
+    # Will give the final output
+
+if response.status_code == 200:
+    print("\nWant to play 'How old am I'?\n\n")
+    answer = input('Type Y or N:> ')
+
+    if answer == 'Y' or answer == 'yes' or answer =='y' or answer == 'YES' or answer == 'Yes':
+        print("\nWell, what are you waiting for? Guess my age:")
+        answer = input('\n> ')
+
+        try:
+            if int(answer) == info["number"]:
+                print("\nOf course. Google, right? \n\n:( ")
+            else:
+                print("\nWrong! I am {} years old.".format(info["number"]))
+                print("\nThat'll be one kidney. I'll take an IOU.")
+        except:
+            print("\nWrong! I am {} years old.".format(info["number"]))
+            print("\nThat'll be one kidney. I'll take an IOU")
+
+    elif answer == 'N' or answer == 'NO' or answer == 'n' or answer == 'no' or answer == 'No':
+        print("\nYou must be a old. \n:(")
+
+    else: print("\nOhhh Yeah...")
+
+else: print("Error: {}\n\ntry again later.".format(response.status_code))
