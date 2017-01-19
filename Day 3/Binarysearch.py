0321be7dc5204a7dbbee87784718ef51fde03b64
YOUR NAMEstevekabbisgit config --global user.email stevekabbis@yahoo.comgit initgit config --global user.name YOUR <stevekabbis@yahoo.com>
2017-01-19 21:13:55 +0300

    I added the code for Binary Numbers

--- Day 3/Binarysearch.py
+++ Day 3/Binarysearch.py
@@ -1,53 +1,53 @@
 class BinarySearch(list):
-	def __init__(self,a,b):
+	def __init__(self,comp1,comp2):
 	  data = []
-	  data.append(b)
+	  data.append(comp2)
 	  list_len = 1
-	  while list_len < a:
-	    data.append(data[list_len -1] + b)
+	  while list_len < comp1:
+	    data.append(data[list_len -1] + comp2)
 	    list_len +=  1
 	  super(BinarySearch,self).__init__(data)
 	  self.length = len(data)
 
 	def search(self,number):
 		count  = 0
-		first  = 0
+		start  = 0
 		self.length = len(self)
-		last  = self.length - 1
-		mid_point = int((last) / 2)
-		obj_location = {'count':'','index':''}
-		while first < mid_point:
+		final  = self.length - 1
+		mid_point = int((final) / 2)
+		pt_location = {'count':'','index':''}
+		while start < mid_point:
 			
-			if (first == mid_point) and (self[first] > number):
+			if (start == mid_point) and (self[start] > number):
 				index = -1 
-				obj_location["count"] = self.length
-				obj_location["index"] = index
-				return obj_location
+				pt_location["count"] = self.length
+				pt_location["index"] = index
+				return pt_location
 			
-			elif self[first] == number:
-				index = first
-				obj_location["count"] = count
-				obj_location["index"] = index
-				return obj_location
+			elif self[start] == number:
+				index = start
+				pt_location["count"] = count
+				pt_location["index"] = index
+				return pt_location
 			elif self[last] == number:
-				index = last
-				obj_location["count"] = count
-				obj_location["index"] = index
-				return obj_location
+				index = final
+				pt_location["count"] = count
+				pt_location["index"] = index
+				return pt_location
 			elif self[mid_point] == number:
 				index = mid_point
-				obj_location["count"] = count
-				obj_location["index"] = index
-				return obj_location
+				pt_location["count"] = count
+				pt_location["index"] = index
+				return pt_location
 			else:
 				if self[mid_point] > number:
-					last = mid_point - 1
-					first = first + 1
-					mid_point = (last + first)//2
+					final = mid_point - 1
+					start = start + 1
+					mid_point = (final + start)//2
 				else:
-					first = mid_point + 1
-					last = last - 1
-					mid_point = ((last + first)//2) + 1
+					start = mid_point + 1
+					final = final - 1
+					mid_point = ((final + start)//2) + 1
 			count += 1
-		obj_location = {'count':count,'index':-1}		
-		return obj_location
+		pt_location = {'count':count,'index':-1}		
+		return pt_location
